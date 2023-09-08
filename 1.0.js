6350062e26111492fdfcb4b18b2cd770ee602b8c
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-08 13:58:18 +0200

    1.2

--- 1.0.js
+++ 1.0.js
@@ -1,4 +1,4 @@
-console.log('hello world');
+
 console.log(10+20);
 console.log(10-20); 
 console.log(10*20);
--- 1.2.js
+++ 1.2.js
@@ -1,4 +1,7 @@
 let name = "Gerard";
 let firstName = "Lambert";
 let city = "Paris";
-console.log("Your name is " + firstName + " " + name + " and you live in " + city + ".");
+console.log("your name is " + firstName  + name +  "and you live in" + city);
+console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
+console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
+console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
