003f0af834348598e2c9d8e97ac761619a5922d7
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-08 14:07:41 +0200

    javalol

--- 1.2.js
+++ 1.2.js
@@ -4,4 +4,5 @@ let city = "Paris";
 console.log("your name is " + firstName  + name +  "and you live in" + city);
 console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
 console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
-console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // template literal
+console.log(`your name is ${firstName} ${name} and you live in ${city}`);   // 
+
