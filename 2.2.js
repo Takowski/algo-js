531984ff49cb22993d2d37fafa7f3c8e29ac3467
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-11 10:16:41 +0200

    function

--- 2.2.js
+++ 2.2.js
@@ -1,19 +1,16 @@
-var min = document.getinput("min");
-var max = document.getElementById("max");
-var current= document.getElementById("current");
-var result = document.getElementById("answer");
-var thatsit =false;
-function checkminmax(){
-  if (current>min && current<max){
-    thatsit=true;
-  }
-  else{
-    thatsit=false;
-  }
-}   
-if (thatsit==true){
-    result = "correct user, current is between min and max";
+let min = document.querySelector('#min');
+let max = document.querySelector('#max');
+let current= document.querySelector('#current');
+let result = document.querySelector('#answer');
+let submit = document.querySelector('.submit');
+
+function check(){
+  if (current.value>min.value && current.value<max.value) {
+   
+    result.textContent = "correct user, current is between min and max";
+  }  
+  
+else {result.textContent = "error, current is not between min and max";}
 }
-else{
-    result = "error, current is not between min and max";
-}
+  
+submit.addEventListener('click', checkminmax);
