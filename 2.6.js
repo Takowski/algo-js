69cf0197fe31e3272843d9ef6f6fd05a21128eca
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-11 11:33:55 +0200

    days

---
+++ 2.6.js
@@ -0,0 +1,26 @@
+let day = prompt("choose a number between 1 and 7 to show the corresponding day of the week");
+
+if (day == 1) {
+    alert("Monday");
+}
+else if (day == 2) {
+    alert("Tuesday");
+}
+else if (day == 3) {
+    alert("Wednesday");
+}
+else if (day == 4) {
+    alert("Thursday");
+}
+else if (day == 5) {
+    alert("Friday");
+}
+else if (day == 6) {
+    alert("Saturday");
+}
+else if (day == 7) {
+    alert("Sunday");
+} 
+else {
+    alert("You did not choose a number between 1 and 7");
+}
