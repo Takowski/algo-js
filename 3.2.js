cd01c63ce7f779f7b55daf52ca5165ca59d5cb29
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-11 13:50:28 +0200

    average value

---
+++ 3.2.js
@@ -0,0 +1,6 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [100, 101, 102];
+
+console.log((arr1.reduce((a, b) => a + b, 0))/arr1.length);
+console.log((arr2.reduce((a, b) => a + b, 0))/arr2.length);
+
