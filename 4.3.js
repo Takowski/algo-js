27c98ed28fcc0cb033988d5130fdf4e52f04d5a6
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-11 17:00:42 +0200

    comment 4.3 et creating 4.4

--- 4.3.js
+++ 4.3.js
@@ -13,6 +13,15 @@ let n = prompt("How many random number do you want ?");
 let showarray = multiRand(n);
 alert(showarray);
 
+// multirand function is called with the number of random numbers we want to generate as a parameter
+// the function rand10() returns an array of random numbers between 1 and 10
+// the user is asked how many random numbers he wants to generate and the answer is stored in a variable n
+// the function multirand() is called with the number entered by the user as a parameter
+// multiRand() returns an array of random numbers between 1 and 10 of length n (the number entered by the user)
+// the nubers generaterd are storred in randomarray by push method
+// showarray is a variable that stores the array returned by the function
+// the array returned by the function is displayed in the console using the alert() function
+// the program is run several times to test it
 
 
 
