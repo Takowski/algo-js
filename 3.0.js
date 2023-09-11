2977b132e71384bedfaa9fec5610f49e153a04f7
Takowski <93830775+Takowski@users.noreply.github.com>
2023-09-11 12:36:35 +0200

    learner of group

---
+++ 3.0.js
@@ -0,0 +1,4 @@
+let group = ["Alexandre Vandewiele","Antoine Lansman", "Bastien Venturi","Carole Gérard","Dorian Vanden Eynde","Elisabeth Leyder","Elodie Ali","Justin Michel","Justine Frigo","Justine Leleu","Kimi Lefort","Layla","Lidwine Careme","Lucas Beauloi","Marie Tara","Mathias Barbier","Okly","Pierre Marien","Robin Piot","Rosalie Boulard","Stephane Comblez","Tim Desmet","Toms","Valentin Lefort","Vens Alexandre","Virginie Dourson"];
+for(let learner of group){
+console.log(learner);
+}
